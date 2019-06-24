53aa7cf2e675bb679a9213ff6df60d40f21cbaf0
jan pungarsek <jan.pungarsek@gmail.com>
2019-06-24 15:17:56 +0200

    Added logic for back and front scan line

--- monoton_mnogokotnik.py
+++ monoton_mnogokotnik.py
@@ -2,7 +2,7 @@ import struct
 import numpy as np
 import matplotlib.pyplot as plt
 from random import randint
-from shapely.geometry import Point, Polygon, LineString
+import shapely.geometry
 
 
 class Point(object):
@@ -15,16 +15,46 @@ class Point(object):
         return "Tocka(%s,%s,%s)" % (self.X, self.Y, self.Klas)
 
 
+def sekanjeDaljic(daljica1, daljica2):
+    D = (((daljica1[1][0])-(daljica1[0][0]))*((daljica2[1][1])-(daljica2[0][1]))) - \
+            (((daljica2[1][0])-(daljica2[0][0]))*((daljica1[1][1])-(daljica1[0][1])))
+
+    A = (((daljica2[1][0])-(daljica2[0][0]))*((daljica1[0][1])-(daljica2[0][1]))) - \
+        (((daljica1[0][0])-(daljica2[0][0]))*((daljica2[1][1])-(daljica2[0][1])))
+
+    B = (((daljica1[1][0])-(daljica1[0][0]))*((daljica1[0][1])-(daljica2[0][1]))) - \
+        (((daljica1[0][0])-(daljica2[0][0]))*((daljica1[1][1])-(daljica1[0][1])))
+
+    if D != 0:
+        Ua = float(A/D)
+        Ub = float(B/D)
+        if 0 < Ua and Ua < 1 and 0 < Ub and Ub < 1:
+            x = float(daljica1[0][0])+Ua*(float(daljica1[1][0])-float(daljica1[0][0]))
+            y = float(daljica1[0][1]) + Ua * (float(daljica1[1][1]) - float(daljica1[0][1]))
+            print("Sekanje", "Daljici se sekata. Presečišče je na koordinatah " + str(x) + "," + str(y) + ".")
+            return Point(x, y)
+        else:
+            return False
+        # elif Ua==1.0 or Ub==-0.0 or Ub==1.0 or Ua==-0.0 or Ub==0.0 or Ua==0.0:
+        # x = float(daljica1[0][0])+Ua*(float(daljica1[1][0])-float(daljica1[0][0]))
+        # y = float(daljica1[0][1])+Ua*(float(daljica1[1][1])-float(daljica1[0][1]))
+        # print("Dotikanje", "Daljici se dotikata, na koordinatah " + str(x) + "," + str(y) + ".")
+        # return Point(x, y)
+    else:
+        return False
+
+
 def preberiBin(izDatoteke, vDatoteko):
     arrTock = np.array((()))
     with open(izDatoteke, "r") as file:
         data = np.fromfile(file, dtype=np.float32)
     reshaped = np.size(data) / 2
     data = np.reshape(data, (int(reshaped), 2))
+    newData = data.tolist()
     for i in range(len(data)):
         p = Point(data[i][0], data[i][1])
         arrTock = np.append(arrTock, p)
-    return arrTock
+    return arrTock, newData
 
 # 1.Klasificiramo vsako oglišče vi glede na njegovega predhodnika vp (= vi-1)
 # in naslednika vn (= vi+1). Naletimo lahko na pet tipov oglišč
@@ -97,7 +127,7 @@ def Klasificiramo(arrTock):
         # Vodoravni Odsek
         if i == (len(arrTock) - 1):
             # Predhodnjik
-            if np.isclose(arrTock[i - 1].Y, arrTock[i].Y, atol=0.000001):
+            if np.isclose(arrTock[i - 1].Y, arrTock[i].Y, atol=0.0001):
                 if arrTock[0].Y < arrTock[i].Y:
                     arrTock[i].Klas = 'hmax'
                     hmax = np.append(hmax, arrTock[i])
@@ -105,7 +135,7 @@ def Klasificiramo(arrTock):
                     arrTock[i].Klas = 'hmin'
                     hmin = np.append(hmin, arrTock[i])
             # Naslednjik
-            elif np.isclose(arrTock[0].Y, arrTock[i].Y, atol=0.000001):
+            elif np.isclose(arrTock[0].Y, arrTock[i].Y, atol=0.0001):
                 if arrTock[i-1].Y < arrTock[i].Y:
                     arrTock[i].Klas = 'hmax'
                     hmax = np.append(hmax, arrTock[i])
@@ -113,7 +143,7 @@ def Klasificiramo(arrTock):
                     arrTock[i].Klas = 'hmin'
                     hmin = np.append(hmin, arrTock[i])
         else:
-            if np.isclose(arrTock[i - 1].Y, arrTock[i].Y, atol=0.000001):
+            if np.isclose(arrTock[i - 1].Y, arrTock[i].Y, atol=0.0001):
                 if arrTock[i+1].Y < arrTock[i].Y:
                     arrTock[i].Klas = 'hmax'
                     hmax = np.append(hmax, arrTock[i])
@@ -121,7 +151,7 @@ def Klasificiramo(arrTock):
                     arrTock[i].Klas = 'hmin'
                     hmin = np.append(hmin, arrTock[i])
             # Naslednjik
-            elif np.isclose(arrTock[i+1].Y, arrTock[i].Y, atol=0.000001):
+            elif np.isclose(arrTock[i+1].Y, arrTock[i].Y, atol=0.0001):
                 if arrTock[i-1].Y < arrTock[i].Y:
                     arrTock[i].Klas = 'hmax'
                     hmax = np.append(hmax, arrTock[i])
@@ -134,8 +164,8 @@ def Klasificiramo(arrTock):
         if j is not None:
             i = j
         if i == (len(arrTock) - 1):
-            if np.isclose(arrTock[i - 1].Y, arrTock[i].Y, atol=0.000001) and \
-                 np.isclose(arrTock[0].Y, arrTock[i].Y, atol=0.000001):
+            if np.isclose(arrTock[i - 1].Y, arrTock[i].Y, atol=0.0001) and \
+                 np.isclose(arrTock[0].Y, arrTock[i].Y, atol=0.0001):
                 arrTock[i].Klas = 'IGN'
                 # index = np.argwhere(arrTock == arrTock[i])
                 # print(index)
@@ -143,8 +173,8 @@ def Klasificiramo(arrTock):
                 # j = i
                 continue
         else:
-            if np.isclose(arrTock[i - 1].Y, arrTock[i].Y, atol=0.000001) and \
-                 np.isclose(arrTock[i + 1].Y, arrTock[i].Y, atol=0.000001):
+            if np.isclose(arrTock[i - 1].Y, arrTock[i].Y, atol=0.0001) and \
+                 np.isclose(arrTock[i + 1].Y, arrTock[i].Y, atol=0.0001):
                 arrTock[i].Klas = 'IGN'
                 # index = np.argwhere(arrTock == arrTock[i])
                 # print(index)
@@ -181,15 +211,21 @@ def Klasificiramo(arrTock):
 #    - vse ostale tipe oglišč prezremo.
 
 
-def zadnjaPremica(arrTock):
+def zadnjaPremica(arrTockSorted, arrTock):
+    arrx = np.array(())
     tocke = np.array(())
     zadnjaPreiskovalnaPremica = np.array(())
-    # randomTocka = randint(0, len(arrTock) - 1)
-    # randomTocka = arrTock[randomTocka]
-    randomTocka = arrTock[0]
-    for i in range(len(arrTock)):
-        if np.isclose(arrTock[i].Y, randomTocka.Y, atol=0.001):
-            tocke = np.append(tocke, arrTock[i])
+    # index = randint(0, len(arrTockSorted) - 1)
+    randomTocka = arrTockSorted[0]
+    # Če smo vzeli zadnjo točko, index = -1, ker ga potem inkrementiramo, ko kličemo funkcijo sprednjaPremica
+    # if index == len(arrTockSorted)-1:
+    #    index = -1
+    # randomTocka = arrrTockSorted[0]
+    # if randomTocka.Klas != 'max' and randomTocka.Klas != 'hmax':
+    #    zadnjaPremica(arrTockSorted, arrTock)
+    for i in range(len(arrTockSorted)):
+        if np.isclose(arrTockSorted[i].Y, randomTocka.Y, atol=0.01):
+            tocke = np.append(tocke, arrTockSorted[i])
     tocke = sorted(tocke, key=lambda point: point.X)
     for tocka in tocke:
         if tocka.Klas != 'IGN':
@@ -198,10 +234,64 @@ def zadnjaPremica(arrTock):
                     np.append(zadnjaPreiskovalnaPremica, tocka)
                 zadnjaPreiskovalnaPremica = \
                     np.append(zadnjaPreiskovalnaPremica, tocka)
-            elif tocka.Klas == 'hmax' or tocka.Klas == 'prevoj':
+            elif tocka.Klas == 'hmax':
                 zadnjaPreiskovalnaPremica = \
                     np.append(zadnjaPreiskovalnaPremica, tocka)
-    return zadnjaPreiskovalnaPremica
+    # Poisci presecisca s stranicami polygona.
+    shapely_line = shapely.geometry.LineString([(-2, randomTocka.Y),
+        (2, randomTocka.Y)])
+    for i in range(len(arrTock)):
+        if i != len(arrTock) - 1:
+            shapely_line1 = shapely.geometry.LineString([(arrTock[i].X, arrTock[i].Y),
+            (arrTock[i + 1].X, arrTock[i + 1].Y)])
+            print(shapely_line.coords[0])
+            print(shapely_line.coords[1])
+            print(shapely_line1.coords[0])
+            print(shapely_line1.coords[1])
+            print('\n')
+            tocka = sekanjeDaljic(shapely_line.coords, shapely_line1.coords)
+            if tocka:
+                arrx = np.append(arrx, tocka.X)
+            """ if shapely_line1.intersects(shapely_line):
+                intersection_line = shapely_line1.intersection(shapely_line)
+                p = Point(intersection_line.x, intersection_line.y)
+                points = np.append(points, p) """
+        else:
+            shapely_line1 = shapely.geometry.LineString([(arrTock[i].X, arrTock[i].Y),
+                (arrTock[0].X, arrTock[0].Y)])
+            print(shapely_line.coords[0])
+            print(shapely_line.coords[1])
+            print(shapely_line1.coords[0])
+            print(shapely_line1.coords[1])
+            print('\n')
+            tocka = sekanjeDaljic(shapely_line.coords, shapely_line1.coords)
+            if tocka:
+                arrx = np.append(arrx, tocka.X)
+            """ if shapely_line1.intersects(shapely_line):
+                intersection_line = shapely_line1.intersection(shapely_line)
+                p = Point(intersection_line.x, intersection_line.y)
+                points = np.append(points, p) """
+    # Zbriši duplikate
+    arrx = np.unique(arrx)
+    K = None
+    # preveri, če je so v arrx slučajno točke polygona
+    for i in range(len(zadnjaPreiskovalnaPremica)):
+        for j in range(len(arrx)):
+            if K is not None:
+                j = K
+            if zadnjaPreiskovalnaPremica[i].X == arrx[j]:
+                idx = np.argwhere(zadnjaPreiskovalnaPremica[i].X == arrx[j])
+                arrx = np.delete(arrx, idx)
+                if j != 0:
+                    K = j - 1
+                else:
+                    K = j
+    for x in arrx:
+        zadnjaPreiskovalnaPremica = np.append(zadnjaPreiskovalnaPremica, Point(x, randomTocka.Y))
+    for point in zadnjaPreiskovalnaPremica:
+        print(point)
+    sprednjaPreiskovalnaPremica = sprednjaPremica(arrTockSorted, arrTock, 1)
+    return zadnjaPreiskovalnaPremica, sprednjaPreiskovalnaPremica
 
 
 # 3.2 Inicializacija sprednje preiskovalne premice poteka sledeče:
@@ -219,28 +309,120 @@ def zadnjaPremica(arrTock):
 #    - in vse ostale tipe prezremo.
 
 
-def sprednjaPremica(arrTock):
+def sprednjaPremica(arrTockSorted, arrTock, idx):
+    arrx = np.array(())
+    points = np.array(())
     tocke = np.array(())
     sprednjaPreiskovalnaPremica = np.array(())
-    randomTocka = arrTock[1]
-    for i in range(len(arrTock)):
-        if np.isclose(arrTock[i].Y, randomTocka.Y, atol=0.001):
-            tocke = np.append(tocke, arrTock[i])
-        # Poisci presecisca s polygonom.
+    sprednjaPreiskovalnaPremica1 = np.array(())
+    randomTocka = arrTockSorted[idx]
+    shapely_line = shapely.geometry.LineString([(-2, randomTocka.Y),
+        (2, randomTocka.Y)])
+    # shapely_polygon = shapely.geometry.Polygon(data)
+    # plt.plot(*shapely_polygon.exterior.xy)
+    # plt.show()
+    # intersection_line = shapely_polygon.intersection(shapely_line)
+    # print(intersection_line)
 
+    # dodaj točke, ki imajo isti y znotraj neke tolerance
+    for i in range(len(arrTockSorted)):
+        if np.isclose(arrTockSorted[i].Y, randomTocka.Y, atol=0.001):
+            sprednjaPreiskovalnaPremica = np.append(sprednjaPreiskovalnaPremica, arrTockSorted[i])
+            continue
+    # Poisci presecisca s stranicami polygona.
+    for i in range(len(arrTock)):
+        if i != len(arrTock) - 1:
+            shapely_line1 = shapely.geometry.LineString([(arrTock[i].X, arrTock[i].Y),
+            (arrTock[i + 1].X, arrTock[i + 1].Y)])
+            print(shapely_line.coords[0])
+            print(shapely_line.coords[1])
+            print(shapely_line1.coords[0])
+            print(shapely_line1.coords[1])
+            print('\n')
+            tocka = sekanjeDaljic(shapely_line.coords, shapely_line1.coords)
+            if tocka:
+                arrx = np.append(arrx, tocka.X)
+            """ if shapely_line1.intersects(shapely_line):
+                intersection_line = shapely_line1.intersection(shapely_line)
+                p = Point(intersection_line.x, intersection_line.y)
+                points = np.append(points, p) """
+        else:
+            shapely_line1 = shapely.geometry.LineString([(arrTock[i].X, arrTock[i].Y),
+                (arrTock[0].X, arrTock[0].Y)])
+            print(shapely_line.coords[0])
+            print(shapely_line.coords[1])
+            print(shapely_line1.coords[0])
+            print(shapely_line1.coords[1])
+            print('\n')
+            tocka = sekanjeDaljic(shapely_line.coords, shapely_line1.coords)
+            if tocka:
+                arrx = np.append(arrx, tocka.X)
+            """ if shapely_line1.intersects(shapely_line):
+                intersection_line = shapely_line1.intersection(shapely_line)
+                p = Point(intersection_line.x, intersection_line.y)
+                points = np.append(points, p) """
+    # Zbriši duplikate
+    arrx = np.unique(arrx)
+    K = None
+    # preveri, če je so v arrx slučajno točke polygona
+    for i in range(len(sprednjaPreiskovalnaPremica)):
+        for j in range(len(arrx)):
+            if K is not None:
+                j = K
+            if sprednjaPreiskovalnaPremica[i].X == arrx[j]:
+                idx = np.argwhere(sprednjaPreiskovalnaPremica[i].X == arrx[j])
+                arrx = np.delete(arrx, idx)
+                K = j
+    for x in arrx:
+        sprednjaPreiskovalnaPremica = np.append(sprednjaPreiskovalnaPremica, Point(x, randomTocka.Y))
+    for point in sprednjaPreiskovalnaPremica:
+        print(point)
+    sprednjaPreiskovalnaPremica = sorted(sprednjaPreiskovalnaPremica, key=lambda point: point.X)
+    for tocka in sprednjaPreiskovalnaPremica:
+        if tocka.Klas != 'IGN':
+            if tocka.Klas == 'min':
+                sprednjaPreiskovalnaPremica1 = \
+                    np.append(sprednjaPreiskovalnaPremica1, tocka)
+                sprednjaPreiskovalnaPremica1 = \
+                    np.append(sprednjaPreiskovalnaPremica1, tocka)
+            elif tocka.Klas == 'hmin' or tocka.Klas == 'prevoj':
+                sprednjaPreiskovalnaPremica1 = \
+                    np.append(sprednjaPreiskovalnaPremica1, tocka)
+            elif tocka.Klas == '':
+                sprednjaPreiskovalnaPremica1 = \
+                    np.append(sprednjaPreiskovalnaPremica1, tocka)
+            else:
+                continue
+    return sprednjaPreiskovalnaPremica1
 
 # Zadnja in sprednja preiskovalna premica bi sedaj morale imeti isto (sodo)
 # stevilo tock! V nasprotnem primeru je nekje prislo do napake
 
-
 if __name__ == "__main__":
-    arrTock = preberiBin('polygon4.bin', 'out.bin')
+    arrTock, data = preberiBin('polygon1.bin', 'out.bin')
+    narisi = arrTock
+    narisi = np.append(narisi, narisi[0])
+    xs = np.array((()))
+    ys = np.array((()))
+    for x in narisi:
+        xs = np.append(xs, x.X)
+        ys = np.append(ys, x.Y)
+    plt.figure()
+    plt.plot(xs, ys)
+    plt.show()
     arrTock = Klasificiramo(arrTock)
-    # Sortiraj tocke glede na X vrednost!
+    polygon = arrTock
+    # Sortiraj tocke glede na Y vrednost!
     arrTock = sorted(arrTock, key=lambda point: point.Y)
-    zadnjaPreiskovalnaPremica = zadnjaPremica(arrTock)
-    for tocka in zadnjaPreiskovalnaPremica:
-        print(tocka)
+    # data = sorted(data, key=lambda tocka: tocka[1])
+    zadnjaPreiskovalnaPremica, sprednjaPreiskovalnaPremica = zadnjaPremica(arrTock, polygon)
+    print('\n')
+    for point in zadnjaPreiskovalnaPremica:
+        print(point)
+    for point in sprednjaPreiskovalnaPremica:
+        print(point)
+    # for tocka in zadnjaPreiskovalnaPremica:
+    #   print(tocka)
     # for tocka in arrTock:
     #   print(tocka)
     # narisi = arrTock
@@ -248,8 +430,8 @@ if __name__ == "__main__":
     # xs = np.array((()))
     # ys = np.array((()))
     # for x in narisi:
-    #     xs = np.append(xs, x.X)
-    #     ys = np.append(ys, x.Y)
+    #   xs = np.append(xs, x.X)
+    #   ys = np.append(ys, x.Y)
     # plt.figure()
     # plt.plot(xs, ys)
     # plt.show()
